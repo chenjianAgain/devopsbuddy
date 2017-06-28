8eb206246100de0d3239f3e3ec780b74b2d125b3
joseph <356960930@qq.com>
2017-06-28 18:27:11 +0800

    test

--- src/main/java/com/josephaws/controller/WelcomeController.java
+++ src/main/java/com/josephaws/controller/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.mkyong;
+package com.josephaws.controller;
 
 import java.util.Map;
 
@@ -13,10 +13,15 @@ public class WelcomeController {
 	@Value("${welcome.message:test}")
 	private String message = "Hello World";
 
-	@RequestMapping("/")
+	@RequestMapping("/welcome")
 	public String welcome(Map<String, Object> model) {
 		model.put("message", this.message);
 		return "welcome";
+	}
+
+	@RequestMapping("/ok")
+	public String ok() {
+		return "bb";
 	}
 
 }
